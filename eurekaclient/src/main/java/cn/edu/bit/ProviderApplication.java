7cbd2b817253824d9aa08d468a508177bfeae1af
weixin_43793603 <chenyujin_2000@163.com>
2022-10-14 22:04:00 +0800

    edit config server.

--- eurekaclient/src/main/java/cn/edu/bit/ProviderApplication.java
+++ eurekaclient/src/main/java/cn/edu/bit/ProviderApplication.java
@@ -2,7 +2,6 @@ package cn.edu.bit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ProviderApplication {
